3fd400f4a46b4187e319d579ddd5b712e9ae6bd0
Kevin <k_keller29@aol.com>
2026-03-02 23:48:15 -0600

    generating a vital file based on extracted audio features

--- main.py
+++ main.py
@@ -40,6 +40,6 @@ def generate_from_audio(input_audio_path: str, output_preset_path: str):
 
 if __name__ == "__main__":
     generate_from_audio(
-        "test-tech-house-pluck.wav",         # your audio file
+        "test-piano.wav",         # your audio file
         "generated-piano-5.vital"    # output preset
     )
